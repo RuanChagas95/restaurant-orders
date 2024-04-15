672d706d3229ada4de09f3ca9bcd0e0e06a4c105
Ruan Chagas <ruanchagas95@gmail.com>
2024-04-15 14:51:21 -0300

    test: req01 Ingredient class

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,21 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (Ingredient, Restriction)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    carne = Ingredient("carne")
+    carne2 = Ingredient("carne")
+    gorgonzola = Ingredient("queijo gorgonzola")
+    assert hash(carne) == hash(carne2)
+    assert hash(carne) != hash(gorgonzola)
+    assert carne == carne2
+    assert carne != gorgonzola
+    assert carne.__repr__() == "Ingredient('carne')"
+    tomate = Ingredient("tomate")
+    assert tomate.name == "tomate"
+    for restriction in carne.restrictions:
+        assert restriction in {
+            Restriction.ANIMAL_DERIVED, Restriction.ANIMAL_MEAT}
+
+    tomate_restrictions = list(tomate.restrictions)
+    assert len(tomate_restrictions) == 0
